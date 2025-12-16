291fb7d8d11de87f622186a180f3b131b00843bb
Gurnoor Singh <gurnoorgsingh@gmail.com>
2025-12-16 12:23:04 +0530

    next config errors

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,23 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- reactStrictMode: true,
- webpack: (config) =&gt; {
- config.resolve.fallback = {
- fs: false,
- path: false,
- os: false,
- crypto: false,
- };
- config.externals.push('pino-pretty', 'lokijs', 'encoding');
- return config;
- },
- images: {
- remotePatterns: [
- {
- protocol: 'https',
- hostname: '**',
- },
- ],
- },
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      fs: false,
+      path: false,
+      os: false,
+      crypto: false,
+    };
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 };
+
 module.exports = nextConfig;
